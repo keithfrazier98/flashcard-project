233595e289c54d95b7b563b550ee969602604040
Keith Frazier <keithers98@gmail.com>
2021-04-17 14:53:25 -0700

    added button iconcs back

--- src/components/EditCardBtn.js
+++ src/components/EditCardBtn.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Link, useRouteMatch } from "react-router-dom";
 import { PencilOutline } from "react-ionicons";
 
-
 function EditCardBtn({ cardId }) {
   const { url } = useRouteMatch();
   return (
-    <Link to={`${url}/cards/${cardId}/edit`} className="btn btn-secondary container">
-      <PencilOutline color={"#00000"} title="edit-card" width="50%" />
-    </Link>
+      <Link
+        to={`${url}/cards/${cardId}/edit`}
+        className="btn btn-secondary container"
+      >
+        <PencilOutline color={"#00000"} title="edit-card" />
+      </Link>
   );
 }
 
